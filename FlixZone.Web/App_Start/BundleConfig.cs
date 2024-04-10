f581dcfdea312c08483d4541665660e484dacdde
Onica Ion <139641184+Onica-Ion@users.noreply.github.com>
2024-04-10 23:09:57 +0300

    test template

--- FlixZone.Web/App_Start/BundleConfig.cs
+++ FlixZone.Web/App_Start/BundleConfig.cs
@@ -10,7 +10,26 @@ namespace FlixZone.Web
      {
           public static void RegisterBundles(BundleCollection bundles)
           {
-
+               //Bootstrap style
+               bundles.Add(new StyleBundle("~/bundles/boostrap/css").Include(
+                    "~/Content/css/bootstrap.min.css",
+                    "~/Content/css/font-awesome.min.css",
+                    "~/Content/css/elegant-icons.css",
+                    "~/Content/css/plyr.css",
+                    "~/Content/css/nice-select.css",
+                    "~/Content/css/owl.carousel.min.css",
+                    "~/Content/css/slicknav.min.css",
+                    "~/Content/css/style.css"));
+               //Bootstrap 
+               bundles.Add(new ScriptBundle("~/bundles/boostrap/js").Include(
+                    "~/Scripts/js/jquery-3.3.1.min.js",
+                    "~/Scripts/js/bootstrap.min.js",
+                    "~/Scripts/js/player.js",
+                    "~/Scripts/js/jquery.nice-select.min.js",
+                    "~/Scripts/js/mixitup.min.js",
+                    "~/Scripts/js/jquery.slicknav.js",
+                    "~/Scripts/js/owl.carousel.min.js",
+                    "~/Scripts/js/main.js"));
           }
      }
 }
